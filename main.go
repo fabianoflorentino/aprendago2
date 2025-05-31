84e1ab1903987fe10cba0d5567460b57bbf3d18a
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-05-31 19:51:09 -0300

    feat: add initialization for loading environment variables using godotenv

--- main.go
+++ main.go
@@ -4,10 +4,17 @@ import (
 	"log"
 
 	"github.com/fabianoflorentino/aprendago2/cmd"
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	if err := cmd.Execute(); err != nil {
 		log.Fatalf("Error executing command: %v", err)
+	}
+}
+
+func init() {
+	if err := godotenv.Load(); err != nil {
+		log.Fatalf("Error loading environment variables: %v", err)
 	}
 }
