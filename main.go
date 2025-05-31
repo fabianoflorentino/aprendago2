2c62aa829d62e2f4bdd1f1260a1d063837d7c46e
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-05-31 20:40:38 -0300

    feat: specify envFile for loading environment variables in init function

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/joho/godotenv"
 )
 
+const envFile = "configs/.env"
+
 func main() {
 	if err := cmd.Execute(); err != nil {
 		log.Fatalf("Error executing command: %v", err)
@@ -14,7 +16,7 @@ func main() {
 }
 
 func init() {
-	if err := godotenv.Load(); err != nil {
+	if err := godotenv.Load(envFile); err != nil {
 		log.Fatalf("Error loading environment variables: %v", err)
 	}
 }
