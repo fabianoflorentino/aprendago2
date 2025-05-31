85c39ba35e8c878af138a8604f776a975a34c343
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-05-31 10:05:20 -0300

    feat: implement root command for Cobra CLI

---
+++ cmd/root.go
@@ -0,0 +1,16 @@
+package cmd
+
+import "github.com/spf13/cobra"
+
+var rootCmd = &cobra.Command{
+	Use:   "apd",
+	Short: "Aprenda Go",
+	Long:  "Aprenda Go com Aprendago é um projeto para ensinar Go de forma simples e prática.",
+}
+
+func Execute() error {
+	if err := rootCmd.Execute(); err != nil {
+		return err
+	}
+	return nil
+}
