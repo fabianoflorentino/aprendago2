0aa23ecada68d6b59a915c0c49fd63ba66748d2b
Fabiano Santos Florentino <fabianoflorentino@outlook.com>
2025-05-31 15:31:40 -0300

    feat: add Run function to root command for help display

--- cmd/root.go
+++ cmd/root.go
@@ -6,11 +6,15 @@ var rootCmd = &cobra.Command{
 	Use:   "apd",
 	Short: "Aprenda Go",
 	Long:  "Aprenda Go com Aprendago é um projeto para ensinar Go de forma simples e prática.",
+	Run: func(cmd *cobra.Command, args []string) {
+		cmd.Help()
+	},
 }
 
 func Execute() error {
 	if err := rootCmd.Execute(); err != nil {
 		return err
 	}
+
 	return nil
 }
